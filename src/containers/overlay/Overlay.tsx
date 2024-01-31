cbf73fcfce82ff6d66af41e09dc2d63d281186e4
jaeyjeon <n8720432@naver.com>
2024-02-01 02:51:18 +0900

    design: overlay 지도목록 버튼 추가 및 지도목록 cardslider 추가 #5

--- src/containers/overlay/Overlay.tsx
+++ src/containers/overlay/Overlay.tsx
@@ -1,6 +1,26 @@
+"use client"
+
 import styles from '@/styles/layout/_overlay.module.scss'
+import CardSlider from '@/components/CardSlider'
+import { useState } from 'react';
 
 export default function Overlay(){
+	const [isCardSliderOn, setIsCardSliderOn] = useState(1);
+	const [cardSliderBtnMsg, setCardSliderBtnMsg] = useState("지도 목록 숨기기 ∨");
+
+	const toggleCardSlider = () => {
+		setIsCardSliderOn((prevState) => {
+		  if (prevState === 1) return 0;
+		  return 1;
+		});
+		if(cardSliderBtnMsg === "지도 목록 숨기기 ∨"){
+			setCardSliderBtnMsg("지도 목록 보이기 ∧");
+		}
+		else{
+			setCardSliderBtnMsg("지도 목록 숨기기 ∨");
+		}
+	  };
+
 	return(
 		<section className={styles.overlay}>
 			<div className={styles.top}>
@@ -20,7 +40,33 @@ export default function Overlay(){
 				</button>
 			</div>
 			<div></div>
-			<div className={styles.bottom}>콜렉션 표시 위치</div>
+			<div className={styles.bottom}>
+				<div>
+					{
+						isCardSliderOn == 1 &&
+						<CardSlider width={900} scrollCardNumber={3}>
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+							<img src="https://picsum.photos/200/300" alt="image" />
+						</CardSlider>
+					}
+				</div>
+				<div className={styles.buttonBox}>
+					<button className={styles.bottomButton}>내 지도</button>
+					<button className={styles.bottomButton}>좋아요한 지도</button>
+					<button className={styles.bottomButton}>새지도 만들기 +</button>
+					<button className={styles.bottomButton} onClick={toggleCardSlider}>{cardSliderBtnMsg}</button>
+				</div>
+			</div>
 		</section>
 	);
 }
