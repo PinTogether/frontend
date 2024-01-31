05a0aa32b40b7e9a27a0f80498c878a7582c9e97
jaeyjeon <n8720432@naver.com>
2024-01-31 20:06:10 +0900

    design: 내위치 버튼추가 및 css수정 #5

--- src/containers/overlay/Overlay.tsx
+++ src/containers/overlay/Overlay.tsx
@@ -1,11 +1,15 @@
 import styles from '@/styles/layout/_overlay.module.scss'
 
-
 export default function Overlay(){
 	return(
 		<section className={styles.overlay}>
 			<div className={styles.top}>
-				서울특별시 강남구 개포동
+				<button className={styles.topButton}>
+					<img src="/icon/location_plain.svg" alt="location button" className={styles.icon}></img>
+				</button>
+				<div className={styles.topLocation}>
+					서울특별시 강남구 개포동
+				</div>
 			</div>
 			<div></div>
 			<div className={styles.bottom}>콜렉션 표시 위치</div>
