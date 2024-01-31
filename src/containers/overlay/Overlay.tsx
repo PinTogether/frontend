568c6d99cbeb2444d277a0cb875a630af2c8c8c0
jaeyjeon <n8720432@naver.com>
2024-02-01 02:13:25 +0900

    design: overlay 디자인 수정 및 효과 추가 #5

--- src/containers/overlay/Overlay.tsx
+++ src/containers/overlay/Overlay.tsx
@@ -4,12 +4,20 @@ export default function Overlay(){
 	return(
 		<section className={styles.overlay}>
 			<div className={styles.top}>
+				<div className={styles.topLocation}>
+					<div>
+						서울특별시
+					</div>
+					<div>
+						강남구
+					</div>
+					<div>
+						개포2동
+					</div>
+				</div>
 				<button className={styles.topButton}>
 					<img src="/icon/location_plain.svg" alt="location button" className={styles.icon}></img>
 				</button>
-				<div className={styles.topLocation}>
-					서울특별시 강남구 개포동
-				</div>
 			</div>
 			<div></div>
 			<div className={styles.bottom}>콜렉션 표시 위치</div>
