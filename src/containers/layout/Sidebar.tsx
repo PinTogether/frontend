0ba004359df874204ecaa304a901f68221973fa3
jaeyjeon <n8720432@naver.com>
2024-02-01 14:29:17 +0900

    design: maincontent css 수정 #5

--- src/containers/layout/Sidebar.tsx
+++ src/containers/layout/Sidebar.tsx
@@ -5,25 +5,25 @@ export default function Sidebar(){
 		<section className={styles.container}>
 			<div></div>
 			<button className={styles.button}>
-				<img src="/icon/home_plain.svg" alt="Home Icon" className={styles.icon}></img>
+				<img src="/icon/home_plain.svg" alt="Home Icon" className={styles.icon} />
 				<img src="/icon/home_hovered.svg" alt="Home Icon Hovered" className={styles.hoveredIcon} />
 			</button>
 			<button className={styles.button}>
-				<img src="/icon/map_plain.svg" alt="map Icon" className={styles.icon}></img>
+				<img src="/icon/map_plain.svg" alt="map Icon" className={styles.icon} />
 				<img src="/icon/map_hovered.svg" alt="Map Icon Hovered" className={styles.hoveredIcon} />
 			</button>
 			<button className={styles.button}>
-				<img src="/icon/like_plain.svg" alt="like Icon" className={styles.icon}></img>
+				<img src="/icon/like_plain.svg" alt="like Icon" className={styles.icon} />
 				<img src="/icon/like_hovered.svg" alt="Like Icon Hovered" className={styles.hoveredIcon} />
 			</button>
 			<div></div>
 			<button className={styles.button}>
-				<img src="/icon/bell_plain.svg" alt="bell Icon" className={styles.icon}></img>
+				<img src="/icon/bell_plain.svg" alt="bell Icon" className={styles.icon} />
 				<img src="/icon/bell_hovered.svg" alt="Bell Icon Hovered" className={styles.hoveredIcon} />
 			</button>
 			<button className={styles.button}>
 				<div className={styles.profilebox}>
-					<img src="/images/cat_dummy.jpeg" alt="profile image" className={styles.profile}></img>
+					<img src="/images/cat_dummy.jpeg" alt="profile image" className={styles.profile} />
 				</div>
 			</button>
 			<div></div>
