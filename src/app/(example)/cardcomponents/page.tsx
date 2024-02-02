172881dc96948990beb70fc6d60f8bce917ec0e4
jwo1024 <dnemtmxlr@gmail.com>
2024-02-03 01:50:36 +0900

    rename: (example) 디렉토리 생성 및 라우팅경로 정리

---
+++ src/app/(example)/cardcomponents/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "@/styles/components/_cardslider.module.scss";
+
+export default function Page() {
+  return (
+    <div className={styles.page}>
+     
+    </div>
+  );
+}
+
