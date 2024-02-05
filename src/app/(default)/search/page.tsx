061bd6db7f9787d031a9de50b44263fe4d05b005
jaeyjeon <n8720432@naver.com>
2024-02-05 16:57:38 +0900

    design: 검색창 디자인 변경 및 설정 추가 #16

--- src/app/(default)/search/page.tsx
+++ src/app/(default)/search/page.tsx
@@ -18,6 +18,11 @@ export default function Page() {
 				<img src="/icon/search_plain.svg" alt="search icon" className={styles.icon} />
 			</button>
 			<input className={styles.input} onChange={onChangeCollection} value={inputCollectionSearch} placeholder="다른 사람의 컬렉션을 검색해 보세요 !  추천 키워드  :  강릉, 맛집,  디저트 ... !"/>
+			<select>
+				<option value="1">컬렉션 검색</option>
+				<option value="2">핀 검색</option>
+				<option value="3">장소 검색</option>
+			</select>
 		</div>
 		<section className={styles.buttonContainer}>
 			<button className={styles.buttons}>장소만 보기</button>
