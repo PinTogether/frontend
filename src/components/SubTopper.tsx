57e3f73ac953b4c82ee2eadb81bce835a5dfa797
jaeyjeon <n8720432@naver.com>
2024-02-10 05:36:25 +0900

    style: 들여쓰기 공백2개로 변경, 주석 제거 #16

--- src/components/SubTopper.tsx
+++ src/components/SubTopper.tsx
@@ -1,14 +1,14 @@
 import styles from "@/styles/components/_subtopper.module.scss"
 
 const Topper = ({ msg }: { msg?: string }) => {
-	return (
-		<section className={styles.topper}>
-			<button>
-				<img src="/icon/expand_left.svg" alt="backward" className={styles.icon} />
-			</button>
-			<p>{msg}</p>
-		</section>
-	);
+  return (
+    <section className={styles.topper}>
+      <button>
+        <img src="/icon/expand_left.svg" alt="backward" className={styles.icon} />
+      </button>
+      <p>{msg}</p>
+    </section>
+  );
   };
 
   export default Topper
