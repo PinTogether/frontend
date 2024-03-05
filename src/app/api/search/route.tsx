540c2ab3fd552b2e7c8439e8ca42e66bc562973f
jwo1024 <dnemtmxlr@gmail.com>
2024-03-05 18:09:07 +0900

    test: test 용 fontend api

---
+++ src/app/api/search/route.tsx
@@ -0,0 +1,16 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import dummydata from "./dummydata.json";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { user_id: string } }
+) {
+  const query = request.nextUrl.searchParams.get("query");
+  const page = request.nextUrl.searchParams.get("page");
+  const size = request.nextUrl.searchParams.get("size");
+  console.log("GET /api/bookmarks/[place_id]/route.tsx");
+  console.log("params", params);
+  console.log("query, page, size", query, page, size);
+  return NextResponse.json(dummydata);
+}
