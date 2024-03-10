8b2b026d320bf7fc3bafce3b6aafe63b8375e094
jwo1024 <dnemtmxlr@gmail.com>
2024-03-10 18:44:50 +0900

    desgin&feat: SubPageLayout위치 내부로 이동, pin deleteButton 추가

--- src/app/(default)/pin/edit/page.tsx
+++ src/app/(default)/pin/edit/page.tsx
@@ -2,9 +2,5 @@ import SubPageLayout from "@/containers/layout/SubPageLayout";
 import PinEditPage from "@/containers/pin/PinEditPage";
 
 export default function Page() {
-  return (
-    <SubPageLayout topperMsg="핀 추가 및 수정">
-      <PinEditPage />
-    </SubPageLayout>
-  );
+  return <PinEditPage />;
 }
