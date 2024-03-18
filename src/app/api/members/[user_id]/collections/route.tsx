843a99bf52ec045b542ca76008bef8772b2abea1
jwo1024 <dnemtmxlr@gmail.com>
2024-03-18 18:25:07 +0900

    test: profile-page에서 쓰이는 test api 작성#73

--- src/app/api/members/[user_id]/collections/route.tsx
+++ src/app/api/members/[user_id]/collections/route.tsx
@@ -6,11 +6,11 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { user_id: string } }
 ) {
-  const offset = request.nextUrl.searchParams.get("offset");
-  const limit = request.nextUrl.searchParams.get("limit");
+  const page = request.nextUrl.searchParams.get("page");
+  const size = request.nextUrl.searchParams.get("size");
   console.log("GET /api/members/[user_id]/collections/route.tsx");
   console.log("params", params);
-  console.log("offset, limit", offset, limit);
+  console.log("page, size", page, size);
   return NextResponse.json({
     status: {
       code: 200,
