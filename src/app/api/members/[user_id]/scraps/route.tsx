c6eb58b35dbbf4ae066dbd9706bdca9ee1892973
jwo1024 <dnemtmxlr@gmail.com>
2024-03-18 15:15:05 +0900

    test: test api #74

--- src/app/api/members/[user_id]/scraps/route.tsx
+++ src/app/api/members/[user_id]/scraps/route.tsx
@@ -8,7 +8,7 @@ export async function GET(
 ) {
   const offset = request.nextUrl.searchParams.get("offset");
   const limit = request.nextUrl.searchParams.get("limit");
-  console.log("GET /api/members/[user_id]/collections/route.tsx");
+  console.log("GET /api/members/[user_id]/scraps/route.tsx");
   console.log("params", params);
   console.log("offset, limit", offset, limit);
   return NextResponse.json({
