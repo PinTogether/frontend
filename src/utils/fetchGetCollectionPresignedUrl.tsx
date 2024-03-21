1a3043aaa515a6f3b0e681b0131d05df9cabe782
jwo1024 <dnemtmxlr@gmail.com>
2024-03-21 14:38:57 +0900

    feat: fetch collection 수정 & 생성 #85

---
+++ src/utils/fetchGetCollectionPresignedUrl.tsx
@@ -0,0 +1,31 @@
+import APIResponse from "@/types/APIResponse";
+import PresignedUrl from "@/types/PresingedUrl";
+
+const fetchGetCollectionPresignedUrl = async (
+  collectionId: number,
+  contentType: string
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/collections/${collectionId}/thumbnail/presigned-url`,
+      {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({ contentType: contentType }),
+      }
+    );
+    console.log("fetchGetCollectionPresignedUrl res", res);
+    if (!res.ok) throw new Error("presinged url 발급에 실패했습니다.");
+    const data: APIResponse = await res.json();
+    console.log("fetchGetCollectionPresignedUrl data", data);
+    const presignedUrlData: PresignedUrl = data.results[0];
+    return { presignedUrlData, errorMessage: "" };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      presignedUrlData: null,
+      errorMessage: "presinged url 발급에 실패했습니다.",
+    };
+  }
+};
+export default fetchGetCollectionPresignedUrl;
---
+++ src/utils/fetchPostCollection.tsx
@@ -0,0 +1,40 @@
+import PresignedUrl from "@/types/PresingedUrl";
+import APIResponse from "@/types/APIResponse";
+
+const fetchPostCollection = async (
+  title: string,
+  thumnail: string,
+  details: string,
+  tags: string[],
+  fileType: string
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/collections`,
+      {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({
+          title: title,
+          thumnail: thumnail,
+          details: details,
+          tags: tags,
+          fileType: fileType,
+        }),
+      }
+    );
+    console.log("fetchPostCollection res", res);
+    if (!res.ok) throw new Error("컬렉션 생성을 실패했습니다.");
+    const data: APIResponse = await res.json();
+    console.log("fetchPostCollection data", data);
+    const presingedUrlData: PresignedUrl = data.results[0];
+    return { presingedUrlData: presingedUrlData, errorMessage: "" };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      presingedUrlData: null,
+      errorMessage: "컬렉션 생성을 실패했습니다.",
+    };
+  }
+};
+export default fetchPostCollection;
