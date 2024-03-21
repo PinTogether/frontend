9ce890c1570154bc47556a3f2d2206e8d074c3d0
jwo1024 <dnemtmxlr@gmail.com>
2024-03-22 01:40:40 +0900

    refact: pinInfo -> pinList 변수명 수정

--- src/utils/fetchGetCollectionAllPins.tsx
+++ src/utils/fetchGetCollectionAllPins.tsx
@@ -13,13 +13,13 @@ const fetchGetCollectionAllPins = async (collectionId: number) => {
     if (!res.ok) throw new Error("핀 리스트 가져오기에 실패했습니다.");
     const data: APIResponse = await res.json();
     console.log("fetchGetCollectionAllPins data", data);
-    const pinInfo: PinForPlace[] = data.results;
-    console.log("pinInfo", pinInfo);
-    return { pinInfo, errorMessage: "" };
+    const pinList: PinForPlace[] = data.results;
+    console.log("pinList", pinList);
+    return { pinList, errorMessage: "" };
   } catch (err: any) {
     console.error(err);
     return {
-      pinInfo: null,
+      pinList: null,
       errorMessage: "핀 리스트F 가져오기에 실패했습니다.",
     };
   }
