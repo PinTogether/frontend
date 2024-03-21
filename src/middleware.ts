a799da747563adcc249dd77cc77a7358fe3c6002
jaeyjeon <n8720432@naver.com>
2024-03-21 16:17:57 +0900

    fix: middleware 적용 및 overlay의 핀 불러오기 변경된 api에 맞게 수정 #63

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // const jwt = request.cookies.get('myCookieName')?.value;
-  // if(!jwt){
-  //   return NextResponse.redirect(new URL("/login", request.url))
-  // }
-  // else{
-  //   //이미 로그인 한 상태에서 login 접속시
-  //   if (request.nextUrl.pathname.startsWith("/login")) {
-  //     return NextResponse.redirect(new URL("/", request.url))
-  //   }
+  const token = request.cookies.get('Authorization')?.value;
+
+  // if(!token && request.nextUrl.pathname.startsWith("/getLogin")){
+  //   const res = NextResponse.redirect((request.url));
+  //   res.cookies.set('Authorization', 'TestAuth');
+  //   return res
   // }
 
+  if(!token && !request.nextUrl.pathname.startsWith("/login")){
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+  else if (token && request.nextUrl.pathname.startsWith("/login")){
+    return NextResponse.redirect(new URL("/", request.url))
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  //matcher: ["/profile/setting/path:*", "/pin/edit/path:*", "/collection/edit/path:*", "/report"]
-  matcher:["/matcherTest"]
+  matcher: ["/profile/setting/:path*", "/pin/edit/:path*", "/collection/edit/:path*", "/report", "/login"] //+ "/getLogin"
 };
