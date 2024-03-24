21ff71a8975ede940cc377d8cdd5c6b805276156
jwo1024 <dnemtmxlr@gmail.com>
2024-03-24 19:17:49 +0900

    feat: placePage 장소정보, 핀 정보 가져오기 및 무한스크롤 적용

---
+++ src/utils/fetchGetPinInfo.tsx
@@ -0,0 +1,31 @@
+import Pin from "@/types/Pin";
+import APIResponse from "@/types/APIResponse";
+
+const fetchGetPinInfo = async (
+  placeId: number
+): Promise<{ pinInfo: Pin | null; errorMessage: string }> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/places/${placeId}`,
+      {
+        credentials: "include",
+      }
+    );
+    console.log("fetchGetPinInfo res", res);
+    if (res.status === 404) {
+      return { pinInfo: null, errorMessage: "장소 정보가 없습니다." };
+    }
+    if (!res.ok) throw new Error("장소 정보 가져오기에 실패했습니다.");
+    const data: APIResponse = await res.json();
+    console.log("fetchGetPinInfo data", data);
+    const pinInfo: Pin = data.results[0];
+    return { pinInfo: pinInfo, errorMessage: "" };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      pinInfo: null,
+      errorMessage: "장소 정보 가져오기에 실패했습니다.",
+    };
+  }
+};
+export default fetchGetPinInfo;
