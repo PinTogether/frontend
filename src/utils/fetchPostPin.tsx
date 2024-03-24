de0d371ddc967663442b602844e35ba7afdb3abd
jwo1024 <dnemtmxlr@gmail.com>
2024-03-24 19:34:18 +0900

    fix&feat: 핀 등록하기 body 데이터 추가, 403 에러처리

--- src/utils/fetchPostPin.tsx
+++ src/utils/fetchPostPin.tsx
@@ -9,8 +9,22 @@ const fetchPostPin = async (
     const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/pins`, {
       method: "POST",
       credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        placeId: placeId,
+        collectionId: collectionId,
+        review: review,
+        tags: tags,
+        fileType: fileType,
+      }),
     });
     console.log("fetchPostPin", res);
+    if (res.status === 403) {
+      return {
+        success: false,
+        errorMessage: "로그인이 필요합니다.",
+      };
+    }
     if (!res.ok) throw new Error("핀 등록에 실패했습니다.");
     return { success: true, errorMessage: "" };
   } catch (err: any) {
