5d12559fbd243aeac54e00bfa85b2be33761a4c7
jwo1024 <dnemtmxlr@gmail.com>
2024-03-24 19:35:40 +0900

    feat: EditPage 로 넘어갈때 pin 정보를 넘길 pinEditSlice 구현

---
+++ src/redux/pinEditSlice.tsx
@@ -0,0 +1,63 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "@/redux/store";
+import Pin from "@/types/Pin";
+
+// Define a type for the slice state
+// interface CounterState {
+//   value: number;
+// }
+
+// Define the initial state using that type
+const initialState: Pin = {
+  id: 0,
+  collectionId: 0,
+  writer: "",
+  review: "",
+  createdAt: "",
+  saveCnt: 0,
+  roadNameAddress: "",
+  placeName: "",
+  longtitude: 0,
+  latitude: 0,
+  starred: false,
+  category: "",
+  tags: [],
+  collectionTitle: "",
+  imagePaths: [],
+};
+
+export const pinEditSlice = createSlice({
+  name: "counter",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState: initialState,
+  reducers: {
+    initialPinEditState: (state, action: PayloadAction<Pin>) => {
+      return action.payload;
+    },
+    clearPinEditState: () => {
+      return initialState;
+    },
+    setPinEditReview: (state, action: PayloadAction<string>) => {
+      state.review = action.payload;
+    },
+    setPinEditTags: (state, action: PayloadAction<string[]>) => {
+      state.tags = action.payload;
+    },
+    setPinEditImagePaths: (state, action: PayloadAction<string[]>) => {
+      state.imagePaths = action.payload;
+    },
+  },
+});
+
+export const {
+  initialPinEditState,
+  clearPinEditState,
+  setPinEditReview,
+  setPinEditTags,
+  setPinEditImagePaths,
+} = pinEditSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectPinEdit = (state: RootState) => state.counter.value;
+
+export default pinEditSlice.reducer;
