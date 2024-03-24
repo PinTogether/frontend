af60865a8f1ec66644820de7e52254dffe42d462
jwo1024 <dnemtmxlr@gmail.com>
2024-03-25 02:13:11 +0900

    feat: 찜하기, 찜 취소하기 api 연결

---
+++ src/utils/fetchPostStarPlace.tsx
@@ -0,0 +1,32 @@
+const fetchPostStarPlace = async (placeId: number) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/stars/${placeId}`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+    console.log("fetchPostStarPlace", res);
+    if (res.status === 403) {
+      return {
+        success: false,
+        errorMessage: "로그인이 필요합니다.",
+      };
+    } else if (res.status === 400 || res.status === 404) {
+      return {
+        success: false,
+        errorMessage: res.statusText,
+      };
+    }
+    if (!res.ok) throw new Error("장소 찜하기 실패했습니다.");
+    return { success: true, errorMessage: "" };
+  } catch (err: any) {
+    console.error(err);
+    return {
+      success: false,
+      errorMessage: "장소 찜하기 실패했습니다.",
+    };
+  }
+};
+export default fetchPostStarPlace;
