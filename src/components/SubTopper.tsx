76563c82727d3c837049fa5d0e4ffb88c7433402
jwo1024 <dnemtmxlr@gmail.com>
2024-03-25 18:13:53 +0900

    fix: img 태그 -> next의 Image태그로 수정

--- src/components/SubTopper.tsx
+++ src/components/SubTopper.tsx
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 
-import styles from "@/styles/components/_subtopper.module.scss"
+import styles from "@/styles/components/_subtopper.module.scss";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Topper = ({ msg }: { msg?: string }) => {
   const router = useRouter();
   return (
     <section className={styles.topper}>
       <button onClick={() => router.back()}>
-        <img src="/icon/expand_left.svg" alt="backward" className={styles.icon} />
+        <Image
+          src="/icon/expand_left.svg"
+          alt="backward"
+          className={styles.icon}
+        />
       </button>
       <b>{msg}</b>
+      {/* <button>완료</button> */}
     </section>
   );
-  };
+};
 
-  export default Topper
+export default Topper;
