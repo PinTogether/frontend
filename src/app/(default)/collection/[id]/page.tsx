c27040937f81dec690c505866913b4b51095a836
jwo1024 <dnemtmxlr@gmail.com>
2024-03-30 03:35:30 +0900

    feat: collectionPage 에  메타데이터 추가

--- src/app/(default)/collection/[id]/page.tsx
+++ src/app/(default)/collection/[id]/page.tsx
@@ -1,9 +1,66 @@
 import CollectionPage from "@/containers/collection/CollectionPage";
+import fetchGetCollectionInfo from "@/utils/collections/fetchGetCollectionInfo";
+import { Metadata } from "next";
 
-type PageParams = {
-  id: number;
-};
+interface PageParams {
+  params: {
+    id: number;
+  };
+}
 
-export default function Page({ params }: { params: PageParams }) {
+export async function generateMetadata({
+  params,
+}: PageParams): Promise<Metadata> {
+  const collectionId = params.id;
+
+  const { collectionInfo, errorMessage } =
+    await fetchGetCollectionInfo(collectionId);
+  if (!collectionInfo) {
+    return {
+      title: "컬렉션 정보를 불러오는 중 오류가 발생했습니다.",
+      description: "컬렉션 정보를 불러오는 중 오류가 발생했습니다.",
+    };
+  }
+  return {
+    title: `${collectionInfo.title}`,
+    description: `${collectionInfo.details}`,
+    applicationName: "pintogether",
+    authors: [
+      {
+        name: `${collectionInfo.writerMembername}`,
+        url: `https://pintogether.com/profile/${collectionInfo.writerId}`,
+      },
+    ],
+    generator: "Next.js 14",
+    keywords: [...collectionInfo.tags, "pintogether", "collection"],
+    // referrer
+    // themeColor
+    // colorScheme
+    // viewport
+    creator: "pintogether team",
+    robots: "index, follow", // 크롤러 탐색
+    // alternate
+    // icons: [
+    //   {
+    //     src: collectionInfo.thumbnail,
+    //     sizes: "192x192",
+    //     type: "image/png",
+    //   },
+    // ],
+    // manifest: {
+
+    openGraph: {
+      title: `${collectionInfo.title}`,
+      description: `${collectionInfo.details} / ${collectionInfo.tags.join(", ")}`,
+      images: [collectionInfo?.thumbnail],
+      type: "website",
+      url: `https://pintogether.com/collection/${collectionId}`,
+      locale: "ko_KR",
+      siteName: "pintogether",
+    },
+  };
+}
+
+export default function Page({ params }: PageParams) {
   return <CollectionPage collectionId={params.id} />;
 }
