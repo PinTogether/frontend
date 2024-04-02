6e3a719acfc3752a5df5cb2908e063d8749e60c7
jwo1024 <dnemtmxlr@gmail.com>
2024-04-03 03:37:48 +0900

    chore: 불필요한 로그출력 삭제

--- src/hooks/useCheckIsMyMembername.tsx
+++ src/hooks/useCheckIsMyMembername.tsx
@@ -4,11 +4,8 @@ const useCheckIsMyMembername = (membername: string) => {
   const myProfileState = useAppSelector((state) => state.myProfile);
 
   try {
-    console.log("myProfileState", myProfileState);
-    console.log("membername", membername);
     if (myProfileState) {
       const decodedMembername = decodeURIComponent(membername);
-      console.log("decodedMembername", decodedMembername);
       return myProfileState.membername == decodedMembername;
     } else {
       return false;
