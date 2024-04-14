f23ff9882dfc6e6a7709a284fb2b07fa54958192
jwo1024 <dnemtmxlr@gmail.com>
2024-04-14 17:54:45 +0900

    Revert "refact: 지도 위치 변화에 따른 리렌더링 방지"
    This reverts commit 71ff0487918cd6d04bdca410b3c8229dafa139c1.

--- src/containers/search/SearchCollectionRenderer.tsx
+++ src/containers/search/SearchCollectionRenderer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import useIntersectionObserver from "@/hooks/useInteresectionObserver";
 import { CollectionDetail } from "@/types/Collection";
 import styles from "@/styles/containers/search/_searchPage.module.scss";
@@ -8,85 +8,86 @@ import { HorizontalDetailCollectionCard } from "@/components/CollectionCard";
 import BouncingLoader from "@/components/BouncingLoader";
 import fetchGetSearchCollection from "@/utils/search/fetchGetSearchCollection";
 
-const SearchCollectionRender = React.memo(
-  ({ searchKeyword }: { searchKeyword: string }) => {
-    const pageNum = useRef(0);
-    const pageEndDiv = useRef<HTMLDivElement>(null);
-    const [collectionDatas, setCollectionDatas] = useState<CollectionDetail[]>(
-      []
-    );
-    const [errorMessage, setErrorMessage] = useState<string>("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [isEnd, setIsEnd] = useState(false);
+export default function SearchCollectionRender({
+  searchKeyword,
+}: {
+  searchKeyword: string;
+}) {
+  const pageNum = useRef(0);
+  const pageEndDiv = useRef<HTMLDivElement>(null);
+  const [collectionDatas, setCollectionDatas] = useState<CollectionDetail[]>(
+    []
+  );
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
 
-    const option = {
-      root: null,
-      rootMargin: "0px", // viewport 기준으로 얼마나 더 감지할 것인가
-      threshold: 0.8, // 0.0 ~ 1.0, 1.0이면 완전히 보이는 상태
-    };
-    const isIntersecting = useIntersectionObserver(pageEndDiv, option);
+  const option = {
+    root: null,
+    rootMargin: "0px", // viewport 기준으로 얼마나 더 감지할 것인가
+    threshold: 0.8, // 0.0 ~ 1.0, 1.0이면 완전히 보이는 상태
+  };
+  const isIntersecting = useIntersectionObserver(pageEndDiv, option);
 
-    useEffect(() => {
-      const resetSearch = () => {
-        pageNum.current = 0;
-        setCollectionDatas([]);
-        setIsEnd(false);
-        setIsLoading(false);
-        setErrorMessage("");
-      };
-      resetSearch();
-    }, [searchKeyword]);
-
-    useEffect(() => {
-      if (isIntersecting && !isLoading && !isEnd) {
-        searchCollection(searchKeyword);
-      }
-    }, [isIntersecting, searchKeyword, isEnd]);
-
-    const searchCollection = async (searchKeyword: string) => {
-      const size = 10;
-      const page = pageNum.current;
-
-      if (isLoading || isEnd) return;
-      setIsLoading(true);
-      const { collectionDatas: newCollectionDatas, errorMessage } =
-        await fetchGetSearchCollection(searchKeyword, page, size);
-      if (newCollectionDatas.length > 0) {
-        setCollectionDatas((prev) => [...prev, ...newCollectionDatas]);
-        pageNum.current += 1;
-      } else {
-        setErrorMessage(errorMessage);
-        setIsEnd(true);
-      }
+  useEffect(() => {
+    const resetSearch = () => {
+      pageNum.current = 0;
+      setCollectionDatas([]);
+      setIsEnd(false);
       setIsLoading(false);
+      setErrorMessage("");
     };
+    resetSearch();
+  }, [searchKeyword]);
 
-    return (
-      <section className={styles.searchListContainer}>
-        {collectionDatas.length === 0 ? (
-          isLoading ? (
-            <BouncingLoader />
-          ) : (
-            <p className={styles.searchMessage}>{errorMessage}</p>
-          )
+  useEffect(() => {
+    if (isIntersecting && !isLoading && !isEnd) {
+      searchCollection(searchKeyword);
+    }
+  }, [isIntersecting, searchKeyword, isEnd]);
+
+  const searchCollection = async (searchKeyword: string) => {
+    const size = 10;
+    const page = pageNum.current;
+
+    if (isLoading || isEnd) return;
+    setIsLoading(true);
+    const { collectionDatas: newCollectionDatas, errorMessage } =
+      await fetchGetSearchCollection(searchKeyword, page, size);
+    if (newCollectionDatas.length > 0) {
+      setCollectionDatas((prev) => [...prev, ...newCollectionDatas]);
+      pageNum.current += 1;
+    } else {
+      setErrorMessage(errorMessage);
+      setIsEnd(true);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <section className={styles.searchListContainer}>
+      {collectionDatas.length === 0 ? (
+        isLoading ? (
+          <BouncingLoader />
         ) : (
-          <>
-            {collectionDatas.map((collectionData, index) => (
-              <HorizontalDetailCollectionCard
-                key={index}
-                collectionData={collectionData}
-              />
-            ))}
-            {isLoading && <BouncingLoader />}
-          </>
-        )}
-        <br />
-        <div
-          ref={pageEndDiv}
-          style={{ height: "5px", display: `${isEnd ? "none" : "block"}` }}
-        ></div>
-      </section>
-    );
-  }
-);
-export default SearchCollectionRender;
+          <p className={styles.searchMessage}>{errorMessage}</p>
+        )
+      ) : (
+        <>
+          {collectionDatas.map((collectionData, index) => (
+            <HorizontalDetailCollectionCard
+              key={index}
+              collectionData={collectionData}
+            />
+          ))}
+          {isLoading && <BouncingLoader />}
+        </>
+      )}
+      <br />
+      <div
+        ref={pageEndDiv}
+        style={{ height: "5px", display: `${isEnd ? "none" : "block"}` }}
+      ></div>
+    </section>
+  );
+}
--- src/containers/search/SearchUserRenderer.tsx
+++ src/containers/search/SearchUserRenderer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import useIntersectionObserver from "@/hooks/useInteresectionObserver";
 import styles from "@/styles/containers/search/_searchPage.module.scss";
 import { ProfileFollower } from "@/types/Profile";
@@ -9,80 +9,84 @@ import fetchGetSearchUser from "@/utils/search/fetchGetSearchUser";
 
 import UserCard from "@/components/UserCard";
 
-const SearchUserRender = React.memo(
-  ({ searchKeyword }: { searchKeyword: string }) => {
-    const pageNum = useRef(0);
-    const pageEndDiv = useRef<HTMLDivElement>(null);
-    const [userDatas, setUserDatas] = useState<ProfileFollower[]>([]);
-    const [errorMessage, setErrorMessage] = useState<string>("");
-    const [isLoading, setIsLoading] = useState(false);
-    const [isEnd, setIsEnd] = useState(false);
+export default function SearchUserRender({
+  searchKeyword,
+}: {
+  searchKeyword: string;
+}) {
+  const pageNum = useRef(0);
+  const pageEndDiv = useRef<HTMLDivElement>(null);
+  const [userDatas, setUserDatas] = useState<ProfileFollower[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
 
-    const option = {
-      root: null,
-      rootMargin: "0px", // viewport 기준으로 얼마나 더 감지할 것인가
-      threshold: 0.8, // 0.0 ~ 1.0, 1.0이면 완전히 보이는 상태
-    };
-    const isIntersecting = useIntersectionObserver(pageEndDiv, option);
+  const option = {
+    root: null,
+    rootMargin: "0px", // viewport 기준으로 얼마나 더 감지할 것인가
+    threshold: 0.8, // 0.0 ~ 1.0, 1.0이면 완전히 보이는 상태
+  };
+  const isIntersecting = useIntersectionObserver(pageEndDiv, option);
 
-    useEffect(() => {
-      const resetSearch = () => {
-        pageNum.current = 0;
-        setUserDatas([]);
-        setIsEnd(false);
-        setIsLoading(false);
-        setErrorMessage("");
-      };
-      resetSearch();
-    }, [searchKeyword]);
-
-    useEffect(() => {
-      if (isIntersecting && !isLoading && !isEnd) {
-        searchUser(searchKeyword);
-      }
-    }, [isIntersecting, searchKeyword, isEnd]);
-
-    const searchUser = async (searchKeyword: string) => {
-      const size = 10;
-      const page = pageNum.current;
-
-      if (isLoading || isEnd) return;
-      setIsLoading(true);
-      const { userDatas: newuserDatas, errorMessage } =
-        await fetchGetSearchUser(searchKeyword, page, size);
-      if (newuserDatas.length > 0) {
-        setUserDatas((prev) => [...prev, ...newuserDatas]);
-        pageNum.current += 1;
-      } else {
-        setErrorMessage(errorMessage);
-        setIsEnd(true);
-      }
+  useEffect(() => {
+    const resetSearch = () => {
+      pageNum.current = 0;
+      setUserDatas([]);
+      setIsEnd(false);
       setIsLoading(false);
+      setErrorMessage("");
     };
+    resetSearch();
+  }, [searchKeyword]);
 
-    return (
-      <section className={styles.searchListContainer}>
-        {userDatas.length === 0 ? (
-          isLoading ? (
-            <BouncingLoader />
-          ) : (
-            <p className={styles.searchMessage}>{errorMessage}</p>
-          )
-        ) : (
-          <>
-            {userDatas.map((userData, index) => (
-              <UserCard key={index} user={userData} showUnfollowButton={true} />
-            ))}
-            {isLoading && <BouncingLoader />}
-          </>
-        )}
-        <br />
-        <div
-          ref={pageEndDiv}
-          style={{ height: "5px", display: `${isEnd ? "none" : "block"}` }}
-        ></div>
-      </section>
+  useEffect(() => {
+    if (isIntersecting && !isLoading && !isEnd) {
+      searchUser(searchKeyword);
+    }
+  }, [isIntersecting, searchKeyword, isEnd]);
+
+  const searchUser = async (searchKeyword: string) => {
+    const size = 10;
+    const page = pageNum.current;
+
+    if (isLoading || isEnd) return;
+    setIsLoading(true);
+    const { userDatas: newuserDatas, errorMessage } = await fetchGetSearchUser(
+      searchKeyword,
+      page,
+      size
     );
-  }
-);
-export default SearchUserRender;
+    if (newuserDatas.length > 0) {
+      setUserDatas((prev) => [...prev, ...newuserDatas]);
+      pageNum.current += 1;
+    } else {
+      setErrorMessage(errorMessage);
+      setIsEnd(true);
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <section className={styles.searchListContainer}>
+      {userDatas.length === 0 ? (
+        isLoading ? (
+          <BouncingLoader />
+        ) : (
+          <p className={styles.searchMessage}>{errorMessage}</p>
+        )
+      ) : (
+        <>
+          {userDatas.map((userData, index) => (
+            <UserCard key={index} user={userData} showUnfollowButton={true} />
+          ))}
+          {isLoading && <BouncingLoader />}
+        </>
+      )}
+      <br />
+      <div
+        ref={pageEndDiv}
+        style={{ height: "5px", display: `${isEnd ? "none" : "block"}` }}
+      ></div>
+    </section>
+  );
+}
